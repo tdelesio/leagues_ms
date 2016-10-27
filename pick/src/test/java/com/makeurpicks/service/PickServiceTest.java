92ea345d0e1a44e424394b93e8a2e41294a8526f
Pawel Nowicki <pawel@Pawels-MacBook-Pro.local>
2016-10-27 17:35:20 -0400

    wip unit tests

--- pick/src/test/java/com/makeurpicks/service/PickServiceTest.java
+++ pick/src/test/java/com/makeurpicks/service/PickServiceTest.java
@@ -1,28 +1,37 @@
 package com.makeurpicks.service;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import java.time.ZonedDateTime;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.UUID;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import com.makeurpicks.PicksApplication;
+import com.makeurpicks.domain.DoublePick;
 import com.makeurpicks.domain.Pick;
 import com.makeurpicks.exception.PickValidationException;
 import com.makeurpicks.exception.PickValidationException.PickExceptions;
 import com.makeurpicks.game.GameIntegrationService;
 import com.makeurpicks.game.GameResponse;
-
-import junit.framework.Assert;
+import com.makeurpicks.repository.DoublePickRepository;
+import com.makeurpicks.repository.PickRepository;
+import com.makeurpicks.repository.PicksByWeekRepository;
 
 @RunWith(MockitoJUnitRunner.class)
 @SpringApplicationConfiguration(classes = PicksApplication.class)
@@ -34,6 +43,18 @@ public class PickServiceTest {
 	
 	@Mock
 	private GameIntegrationService gameIntegrationMock;
+	
+	@Mock
+	private PickRepository pickRepositoryMock;
+
+	@Mock
+	private DoublePickRepository doublePickRepositoryMock;
+	
+	@Mock
+	private PicksByWeekRepository picksByWeekRepositoryMock;
+	
+	@Rule
+	public ExpectedException expectedEx = ExpectedException.none();
 	
 	@Test
 	public void makePick_ValidateIdIsSet() {
@@ -178,7 +199,7 @@ public class PickServiceTest {
 		}
 		catch (PickValidationException exception)
 		{
-			System.out.println(exception);
+//			System.out.println(exception);
 			assertTrue(exception.hasSpecificException(PickExceptions.TEAM_NOT_PLAYING_IN_GAME));
 		}
 	}
@@ -209,24 +230,168 @@ public class PickServiceTest {
 		}
 		catch (PickValidationException exception)
 		{
-			System.out.println(exception);
+//			System.out.println(exception);
 			assertTrue(exception.hasSpecificException(PickExceptions.GAME_HAS_ALREADY_STARTED));
 		}
 	}
+		
 	
-	
-	
-	
+	@Test
+	public void updatePick_pickNotFound_PickValidationExceptionThrown() {
+		
+		expectedEx.expect(RuntimeException.class);
+	    expectedEx.expectMessage(PickExceptions.PICK_IS_NULL.toString());
+	    
+		Pick pick = new Pick();
+		String pickId = UUID.randomUUID().toString();
+		pick.setId(pickId);
+		String gameId = UUID.randomUUID().toString();
+		pick.setGameId(gameId);
+		String teamId = UUID.randomUUID().toString();
+		pick.setTeamId(teamId);
+		pick.setWeekId(UUID.randomUUID().toString());
+		pick.setLeagueId(UUID.randomUUID().toString());
+		pick.setPlayerId(UUID.randomUUID().toString());
 
-//	@Test
-//	public void testUpdatePick() {
-//		fail("Not yet implemented");
-//	}
-//
-//	@Test
-//	public void testGetPicksByWeekAndPlayer() {
-//		fail("Not yet implemented");
-//	}
+		GameResponse gameResponse = new GameResponse();
+		gameResponse.setId(gameId);
+		gameResponse.setFavId(teamId);
+		gameResponse.setDogId(UUID.randomUUID().toString());
+		gameResponse.setGameStart(ZonedDateTime.now().plusDays(3));
+		
+		when(gameIntegrationMock.getGameById(gameId)).thenReturn(gameResponse);
+		when(pickRepositoryMock.findOne(pickId)).thenReturn(null);
+		
+		service.updatePick(pick);
+	}
+	
+	@Test()
+	public void updatePick_gameIsADouble_deleteDouble() {
+	    
+		Pick pick = new Pick();
+		
+		String playerId = UUID.randomUUID().toString();
+		String pickId = UUID.randomUUID().toString();
+		String gameId = UUID.randomUUID().toString();
+		String teamId = UUID.randomUUID().toString();
+		String leagueId = UUID.randomUUID().toString();
+		String weekId = UUID.randomUUID().toString();
+		
+		pick.setPlayerId(playerId);
+		pick.setId(pickId);
+		pick.setGameId(gameId);
+		pick.setTeamId(teamId);
+		pick.setWeekId(weekId);
+		pick.setLeagueId(leagueId);
+
+		Pick pickFromDS = new Pick();
+		pickFromDS.setPlayerId(playerId);
+		
+		GameResponse gameResponse = new GameResponse();
+		gameResponse.setId(gameId);
+		gameResponse.setFavId(teamId);
+		gameResponse.setDogId(UUID.randomUUID().toString());
+		gameResponse.setGameStart(ZonedDateTime.now().plusDays(1));
+		
+		DoublePick doublePick = new DoublePick();
+		doublePick.setPickId(pickId);
+		
+		when(gameIntegrationMock.getGameById(gameId)).thenReturn(gameResponse);		
+		when(pickRepositoryMock.findOne(pickId)).thenReturn(pickFromDS);
+		when(doublePickRepositoryMock.findDoubleForPlayer(leagueId, weekId, playerId)).thenReturn(doublePick);
+		
+		service.updatePick(pick);
+		
+		verify(doublePickRepositoryMock).delete(doublePick);
+	}
+	
+	@Test()
+	public void updatePick_saveGame_pass() {
+		
+		String playerId = UUID.randomUUID().toString();
+		String pickId = UUID.randomUUID().toString();
+		String gameId = UUID.randomUUID().toString();
+		String teamId = UUID.randomUUID().toString();
+		String leagueId = UUID.randomUUID().toString();
+		String weekId = UUID.randomUUID().toString();
+		
+		Pick pick = new Pick();
+		pick.setPlayerId(playerId);
+		pick.setId(pickId);
+		pick.setGameId(gameId);
+		pick.setTeamId(teamId);
+		pick.setWeekId(weekId);
+		pick.setLeagueId(leagueId);
+
+		Pick pickFromDS = new Pick();
+		pickFromDS.setPlayerId(playerId);
+		
+		GameResponse gameResponse = new GameResponse();
+		gameResponse.setId(gameId);
+		gameResponse.setFavId(teamId);
+		gameResponse.setDogId(UUID.randomUUID().toString());
+		gameResponse.setGameStart(ZonedDateTime.now().plusDays(1));
+		
+		DoublePick doublePick = new DoublePick();
+		doublePick.setPickId(pickId);
+		
+		when(gameIntegrationMock.getGameById(gameId)).thenReturn(gameResponse);		
+		when(pickRepositoryMock.findOne(pickId)).thenReturn(pickFromDS);
+		
+		service.updatePick(pick);
+		
+		verify(pickRepositoryMock).save(pick);
+	}
+	
+	
+	@Test
+	public void getPicksByWeekAndPlayerTest_mapFromDSisNull_returnEmptyMap() {
+		
+		String leagueId = UUID.randomUUID().toString();
+		String weekId = UUID.randomUUID().toString();
+		String playerId = UUID.randomUUID().toString();
+		
+		Pick pick = new Pick();
+		pick.setPlayerId(playerId);
+		pick.setWeekId(weekId);
+		pick.setLeagueId(leagueId);
+		
+		when(picksByWeekRepositoryMock.getPlayersByWeek(leagueId, weekId)).thenReturn(null);
+		
+		Map<String, Pick> map = service.getPicksByWeekAndPlayer(leagueId, weekId, playerId);
+		
+		assertTrue(map.size() == 0);
+	}
+	
+	@Test
+	public void getPicksByWeekAndPlayerTest_mapFromDSisNul_returnEmptyMap() {
+		
+		String leagueId = UUID.randomUUID().toString();
+		String weekId = UUID.randomUUID().toString();
+		String playerId = UUID.randomUUID().toString();
+		String gameId = UUID.randomUUID().toString();
+		String pickId = UUID.randomUUID().toString();
+		
+		Pick pick = new Pick();
+		pick.setId(pickId);
+		pick.setPlayerId(playerId);
+		pick.setWeekId(weekId);
+		pick.setLeagueId(leagueId);
+		
+		Map<String, Map<String, String>> playersByWeekMap = new HashMap<>();
+		Map<String, String> gamesMap = new HashMap<>();
+		Map<String, Pick> pickMap = new HashMap<>();
+		
+		pickMap.put(gameId, pick);
+		
+		playersByWeekMap.put(playerId, gamesMap);
+		
+		when(picksByWeekRepositoryMock.getPlayersByWeek(leagueId, weekId)).thenReturn(playersByWeekMap);
+		
+		Map<String, Pick> map = service.getPicksByWeekAndPlayer(leagueId, weekId, playerId);
+		
+		assertTrue(map.size() == 1);
+	}
 //
 //	@Test
 //	public void testGetOtherPicksByWeekAndPlayer() {
@@ -251,6 +416,40 @@ public class PickServiceTest {
 //	@Test
 //	public void testMakeDoublePick() {
 //		fail("Not yet implemented");
+//	}
+
+//	public class DoublePickRepositorySpy implements DoublePickRepository {
+//
+//		public boolean deleteWasCalled = false;
+//		
+//		@Override
+//		public Map<String, DoublePick> findAllForLeagueAndWeek(String leagueId, String weekId) {
+//			// TODO Auto-generated method stub
+//			return null;
+//		}
+//	
+//		@Override
+//		public DoublePick findDoubleForPlayer(String leagueId, String weekId, String playerId) {
+//			// TODO Auto-generated method stub
+//			return null;
+//		}
+//	
+//		@Override
+//		public void delete(DoublePick doublePick) {
+//			deleteWasCalled = true;
+//		}
+//	
+//		@Override
+//		public void save(DoublePick doublePick) {
+//			// TODO Auto-generated method stub
+//			
+//		}
+//	
+//		@Override
+//		public void deleteAll() {
+//			// TODO Auto-generated method stub
+//			
+//		}
 //	}
 
 }
