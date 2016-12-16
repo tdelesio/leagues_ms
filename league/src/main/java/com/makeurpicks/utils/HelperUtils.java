a40e802f353d291813b2d9f9f60622658767b9ec
dinesh-techolution <dinesh.challa@techolution.com>
2016-12-16 22:55:35 +0530

    added helper utils

---
+++ league/src/main/java/com/makeurpicks/utils/HelperUtils.java
@@ -0,0 +1,20 @@
+package com.makeurpicks.utils;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+
+import com.makeurpicks.domain.League;
+import com.makeurpicks.domain.LeagueName;
+
+public class HelperUtils {
+	
+	public static Set<LeagueName> getLeagueNameFromLeagues(Collection<League> leagues) {
+		Set<LeagueName> leagueNames = new HashSet<LeagueName>();
+		leagues.forEach(e-> {
+			leagueNames.add(new LeagueName(e));
+		});
+		leagueNames.add(new LeagueName());
+		return leagueNames;
+	}
+}
